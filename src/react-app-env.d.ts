145a5586e08b5f3b6b11687647957c69bc012c6c
atuikhar <atuikhar@gmail.com>
2023-08-05 17:57:42 +0530

    vite env fix

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,2 +1,11 @@
 /// <reference types="react-scripts" />
 /// <reference types="vite/client" />
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_REACT_APP_CLERK_PUBLISHABLE_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
